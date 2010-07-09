32ae026ede376e4c1c80e9076b91fd64af73eaac
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-09 18:09:26 +0000

    Added a file remotely
    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/trunk@262035 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ optimizer-tracking.js
@@ -0,0 +1,7 @@
+if(typeof(_gat)!='object')document.write('<sc'+'ript src="http'+
+(document.location.protocol=='https:'?'s://ssl':'://www')+
+'.google-analytics.com/ga.js"></sc'+'ript>');
+try {
+var gwoTracker=_gat._getTracker(optimizer_tracking_id);
+gwoTracker._trackPageview(optimizer);
+}catch(err){};
