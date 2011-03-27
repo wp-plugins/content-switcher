c818fa8e50c02b023b06d16bfe7104973b6fafb7
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-27 17:45:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/trunk@365461 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,16 @@
+<?php function content_switcher_options_page() { include 'options-page.php'; }
+
+function content_switcher_admin_menu() {
+add_options_page('Content Switcher', 'Content Switcher', 'manage_options', 'content-switcher', 'content_switcher_options_page'); }
+
+add_action('admin_menu', 'content_switcher_admin_menu');
+
+
+function content_switcher_row_meta($links, $file) {
+if ($file == 'content-switcher/content-switcher.php') {
+return array_merge($links, array(
+'<a href="options-general.php?page=content-switcher">'.__('Options', 'content-switcher').'</a>',
+'<a href="http://www.kleor-editions.com/'.__('content-switcher/en', 'content-switcher').'">'.__('Documentation', 'content-switcher').'</a>')); }
+return $links; }
+
+add_filter('plugin_row_meta', 'content_switcher_row_meta', 10, 2);
