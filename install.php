f9ad78e18e8f71e07cba3a0de0cd18b56a107ba7
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-18 04:57:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/tags/1.7.5@386112 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php $content_switcher_default_options = array(
+'analytics_tracking_admin' => 'no',
+'analytics_tracking_author' => 'yes',
+'analytics_tracking_contributor' => 'yes',
+'analytics_tracking_editor' => 'yes',
+'analytics_tracking_id' => '',
+'analytics_tracking_subscriber' => 'yes',
+'analytics_tracking_visitor' => 'yes',
+'javascript_enabled' => 'no',
+'optimizer_tracking_id' => '');
+
+$content_switcher_options = get_option('content_switcher');
+foreach ($content_switcher_default_options as $key => $value) {
+if ($content_switcher_options[$key] == '') { $content_switcher_options[$key] = $content_switcher_default_options[$key]; } }
+update_option('content_switcher', $content_switcher_options);
