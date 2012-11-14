48c4c385a6d37b3a7249997ea6d6df56725217af
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-14 03:25:55 +0000

    * Version 3.2 	* Improve the structure of files
    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/tags/3.2@625017 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ install.php
@@ -0,0 +1,8 @@
+<?php load_plugin_textdomain('content-switcher', false, 'content-switcher/languages');
+include dirname(__FILE__).'/initial-options.php';
+$options = (array) get_option('content_switcher');
+$current_options = $options;
+if ((isset($options[0])) && ($options[0] === false)) { unset($options[0]); }
+foreach ($initial_options as $key => $value) {
+if (($key == 'version') || (!isset($options[$key])) || ($options[$key] == '')) { $options[$key] = $value; } }
+if ($options != $current_options) { update_option('content_switcher', $options); }
