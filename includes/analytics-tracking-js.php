d85b8f510be21c8ce49b552e1303b808b7f936e3
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-02 15:16:56 +0000

    Change languages files
    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/tags/3.4.1@765793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/analytics-tracking-js.php
+++ includes/analytics-tracking-js.php
@@ -1,4 +1,6 @@
-<?php $analytics_tracking = false;
+<?php $analytics_tracking_id = content_switcher_data('analytics_tracking_id');
+if ($analytics_tracking_id != '') {
+$analytics_tracking = false;
 if (current_user_can('manage_options')) { if (content_switcher_data('administrator_tracked') == 'yes') { $analytics_tracking = true; } }
 elseif (current_user_can('edit_pages')) { if (content_switcher_data('editor_tracked') == 'yes') { $analytics_tracking = true; } }
 elseif (current_user_can('publish_posts')) { if (content_switcher_data('author_tracked') == 'yes') { $analytics_tracking = true; } }
@@ -8,7 +10,7 @@ else { if (content_switcher_data('visitor_tracked') == 'yes') { $analytics_track
 if ($analytics_tracking) { ?>
 <script type="text/javascript">
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '<?php echo content_switcher_data('analytics_tracking_id'); ?>']);
+_gaq.push(['_setAccount', '<?php echo $analytics_tracking_id; ?>']);
 _gaq.push(['_trackPageview']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
@@ -16,4 +18,4 @@ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www')
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
-<?php }
+<?php } }
