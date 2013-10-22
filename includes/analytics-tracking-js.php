c70de6fc0610b5c7af874e3a96228deaf603b7a8
Kleor <Kleor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-22 03:52:11 +0000

    * Version 3.5.1 	* Improve the shortcodes
    git-svn-id: https://plugins.svn.wordpress.org/content-switcher/tags/3.5.1@791587 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/analytics-tracking-js.php
@@ -0,0 +1,21 @@
+<?php $analytics_tracking_id = content_switcher_data('analytics_tracking_id');
+if ($analytics_tracking_id != '') {
+$analytics_tracking = false;
+if (current_user_can('manage_options')) { if (content_switcher_data('administrator_tracked') == 'yes') { $analytics_tracking = true; } }
+elseif (current_user_can('edit_pages')) { if (content_switcher_data('editor_tracked') == 'yes') { $analytics_tracking = true; } }
+elseif (current_user_can('publish_posts')) { if (content_switcher_data('author_tracked') == 'yes') { $analytics_tracking = true; } }
+elseif (current_user_can('edit_posts')) { if (content_switcher_data('contributor_tracked') == 'yes') { $analytics_tracking = true; } }
+elseif (current_user_can('read')) { if (content_switcher_data('subscriber_tracked') == 'yes') { $analytics_tracking = true; } }
+else { if (content_switcher_data('visitor_tracked') == 'yes') { $analytics_tracking = true; } }
+if ($analytics_tracking) { ?>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '<?php echo $analytics_tracking_id; ?>']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+<?php } }
